2fb767a8c1b768a3ea660b24b5e56d443b41fcc5
Amine Mouafik <amine@mouafik.fr>
2018-04-04 14:25:12 +0700

    Apply standard formatting for tests to pass

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-'use strict';
+'use strict'
 
-let Url = require('url');
+let Url = require('url')
 
 let plugin = {
-  register: function(server, options) {
+  register: function (server, options) {
     server.ext('onRequest', function (request, h) {
-      let defaultOpts = {https: true };// default www redirects
-      let opts = Object.assign(defaultOpts, options); 
-      let host = request.headers.host;
-      let protocol = request.server.info.protocol;
-      let forwardedProtocol = request.headers['x-forwarded-proto'];
-      let isHttpProtocol = opts.proxy ? forwardedProtocol === 'http' : protocol === 'http';
-      let redirect = false;
+      let defaultOpts = { https: true }// default www redirects
+      let opts = Object.assign(defaultOpts, options)
+      let host = request.headers.host
+      let protocol = request.server.info.protocol
+      let forwardedProtocol = request.headers['x-forwarded-proto']
+      let isHttpProtocol = opts.proxy ? forwardedProtocol === 'http' : protocol === 'http'
+      let redirect = false
 
-      if((opts.www && !/^www\./.test(host)) || (opts.nonwww && /^www\./.test(host))) {
-          host = opts.www ? 'www.' + host : host.replace(/^www\./, '');
-          redirect = true;
+      if ((opts.www && !/^www\./.test(host)) || (opts.nonwww && /^www\./.test(host))) {
+        host = opts.www ? 'www.' + host : host.replace(/^www\./, '')
+        redirect = true
       }
-      
+
       // https redirects
-      if (opts.https && isHttpProtocol){
-          protocol = 'https';
-          redirect = true;
+      if (opts.https && isHttpProtocol) {
+        protocol = 'https'
+        redirect = true
       }
 
       if (redirect) {
-              return h
+        return h
                   .redirect(Url.format({protocol: protocol, hostname: host, pathname: request.url.pathname, search: request.url.search}))
                   .takeover()
-                  .code(301);
+                  .code(301)
       }
 
-      return h.continue;
-  });
-  //next();
+      return h.continue
+    })
+  // next();
   }
-};
+}
 
-plugin.pkg = require('./package.json');
+plugin.pkg = require('./package.json')
 
-module.exports = plugin; 
+module.exports = plugin
